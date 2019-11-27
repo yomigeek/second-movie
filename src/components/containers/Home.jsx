6e256a52a1450f336d0ca193542b08cf26f1b96b
Oyomi <seayomi@gmail.com>
2019-11-27 22:30:44 +0100

    app setup

--- src/components/containers/Home.jsx
+++ src/components/containers/Home.jsx
@@ -1,5 +1,4 @@
 import React, { Component, Fragment } from 'react';
-import { Navbar, NavItem, Nav, NavDropdown, MenuItem } from 'react-bootstrap';
 import '../../styles/app.scss';
 
 class Home extends Component {
@@ -8,29 +7,7 @@ class Home extends Component {
     return(
       <Fragment>
         <div className="app-text">
-
-                  <Navbar>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="#home">React-Bootstrap</a>
-            </Navbar.Brand>
-          </Navbar.Header>
-          <Nav>
-            <NavItem eventKey={1} href="#">
-              Link
-            </NavItem>
-            <NavItem eventKey={2} href="#">
-              Link
-            </NavItem>
-            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-              <MenuItem eventKey={3.1}>Action</MenuItem>
-              <MenuItem eventKey={3.2}>Another action</MenuItem>
-              <MenuItem eventKey={3.3}>Something else here</MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey={3.4}>Separated link</MenuItem>
-            </NavDropdown>
-          </Nav>
-        </Navbar>
+          welcome to stream
         </div>
       </Fragment>
     )
